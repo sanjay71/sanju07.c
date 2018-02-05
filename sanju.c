ed7521915fc827683f64c91ec4e6b3d09aa1ada1
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-05 16:16:44 +0530

    sanju.c

--- sanju.c
+++ sanju.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
+#iclude<conio.h>
 int main()
 {
-     int a;
-     printf("enter the year");
-     scanf("%d",&a);
-     if(a/4)
-     {
-         printf("year is a leap");
-     }
-    else
-    {
-        printf("year is not leap");
-    }
+ int a;
+  clrscr();
+ printf("enter the year");
+ scanf("%d",&a);
+ if(a/4)
+ {
+ printf("year is a leap");
+  }
+ else
+ {
+ printf("year is not leap");
+ }
+ getch();
+ return 0;
 }
