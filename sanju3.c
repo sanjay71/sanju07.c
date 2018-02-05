e248ed772d3fe4e495d4bccb0c8305d909aa4586
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-05 16:26:39 +0530

    sanju3.c

---
+++ sanju3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#uinclude<conio.h>
+int main()
+{
+int i,n;
+printf("Enter the positive integer:");
+scanf("%d",n);
+for(i=1)
+ {
+ sum += i;  
+ }
+ printf("Sum = %d",sum);
+ getch();
+ return 0;
+ }
