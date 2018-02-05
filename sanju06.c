8bb6a2bb486e973afc11c7c09c84522c703ea6a1
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-05 16:53:12 +0530

    sanju06.c

---
+++ sanju06.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i, n;
+clrscr();
+count=0;
+printf("Enter any number:");
+scanf("%d",&num);
+while(num!==0)
+{
+count++;
+num =0;
+}
+printf("total digits:%d",count);
+getch();
+return o;
+}
