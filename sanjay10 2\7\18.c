385acf465fac34e9d1806978ecf75d8f45ea3a11
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-07 16:51:23 +0530

    sanjay10 2\7\18.c

---
+++ sanjay10 2\7\18.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+int num,r,sum=0,temp;
+printf("enter a 3 digit number");
+scanf("%d",&num);
+temp=num;
+while(num=0)
+{
+r=num%10;
+num=num\10;
+sum==sum(r+r+r);
+}
+if(sum==temp)
+{
+printf(" amstrong number");
+else
+printf("not a amstrong number");
+}
+getch();
+return 0;
+}
+
+
