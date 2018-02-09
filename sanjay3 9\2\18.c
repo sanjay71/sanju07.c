6578ea0d557012c83ccedbde3238e59c54661351
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-09 14:26:35 +0530

    sanjay3 9\2\18.c

---
+++ sanjay3 9\2\18.c
@@ -0,0 +1,17 @@
+include<stdio.h>
+void main()
+{
+int n, m,i;
+clrscr();
+	printf("enter two numbers");
+	scanf("%d%d",&n,&m);
+	for(i=n+1;i<m;i++)
+	{
+	if(i%2==0)
+	{
+		printf("%d\t",i);	
+	}	
+	return 0;
+  getch ();
+  }
+  }
