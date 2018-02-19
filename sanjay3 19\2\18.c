38f3abeb513cb87dfeb9dca63a77da2bcc3848a3
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-02-19 16:31:10 +0530

    sanjay3 19\2\18.c

---
+++ sanjay3 19\2\18.c
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+scanf("%d%d%d",&a,&b,&c);
+if(a<b)&&(a<c)
+{
+printf("a is lesser");
+}
+if(b<c)&&(b<a);
+{
+prpntf("b is lesser");
+}
+else
+printf("c is lesser");
+getch();
+return 0;
+}
