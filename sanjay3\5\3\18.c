177b5a537422ee80753baadefe36b9e4e88670d9
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-05 15:55:00 +0530

    sanjay3\5\3\18.c

---
+++ sanjay3\5\3\18.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int i, n,first=0,next=1,sum;
+clrscr();
+printf("Enter series range:");
+scanf("%d",&n);
+printf("Fibonacci Series:");
+for(i=0;i<n;i++)
+{
+printf("\n%d",first);
+sum=first+next;
+first=next;
+next=sum;
+}
+getch();
+}
