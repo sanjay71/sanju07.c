0d89e76ca712b97b79fbdc7385daacc107a69a08
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-16 15:45:22 +0530

    anan1\16\3\18.c

---
+++ anan1\16\3\18.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+clrscr();
+{
+	char str[20];
+	int k,i;
+	printf("enter the string and the k value:");
+	scanf("%s %d",str,&k);
+	for(i=0;i<k;i++)
+	{
+		printf("\n%c",str[i]);
+	}
+	return 0;
+  getch();
+}
