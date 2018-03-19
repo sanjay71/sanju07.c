7be5d60ab4443bfb9a1890c9f57b8bcdb2c515dc
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-19 15:45:26 +0530

    an1\19\3\18.c

---
+++ an1\19\3\18.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main(void)
+
+{
+
+char a[10];
+clrscr();
+scanf("%s",a);
+int i,flag=0,m;
+m=strlen(a);
+for(i=0;i<m;i++)
+{
+if(a[i]=='1' || a[i]=='0')
+{
+flag=1;
+}
+else
+{
+flag=0;
+break;
+}
+}
+if(flag==1)
+{
+printf("yes");
+}
+getch();
+}
