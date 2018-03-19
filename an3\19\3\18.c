495640ca633fd3743b4289ca1f22b0cc04bc869f
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-19 15:53:15 +0530

    an3\19\3\18.c

--- an3\19\3\18.c
+++ an3\19\3\18.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+ int main(void) 
+{
+int n1,n2,sum;
+clrscr();
+printf("enter the number1:");
+scanf("%d",&n1);
+printf("enter the number2:");
+scanf("%d",&n2);
+sum=n1+n2;
+if(sum%2==0)
+{
+printf("\neven");
+}
+else
+{
+printf("\nodd");
+}
+return 0;
+getch();
+}
