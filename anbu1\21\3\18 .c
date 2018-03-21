5b95f17292fb62beca09c4b90f0f63d78d23233d
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-21 16:13:15 +0530

    anbu1\21\3\18.c

---
+++ anbu1\21\3\18 .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int num;
+  clrscr();
+	printf("enter the number:");
+	scanf("%d",&num);
+	num+=1;
+	while(num%10!=0)
+	{
+		num=num+1;
+	}
+	printf("\n%d",num);
+	return 0;
+  getch();
+}
