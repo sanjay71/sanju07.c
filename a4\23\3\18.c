683386d374ec2e2b3592d77d7a2b24386bc953ae
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-23 03:19:39 -0700

    a4\23\3\18.c

---
+++ a4\23\3\18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+clrscr();
+{
+	float num;
+	int x;
+	printf("\n enter the num:");
+	scanf("%f",&num);
+	x=(int)(num+0.5);
+	printf("\n%d",x);
+	return 0;
+  getch();
+}
