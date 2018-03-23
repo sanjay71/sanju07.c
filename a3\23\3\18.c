9f0b1077af8327ab58d26505861b6f40cb6e817c
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-03-23 03:17:49 -0700

    a3\23\3\18.c

---
+++ a3\23\3\18.c
@@ -0,0 +1,19 @@
+int main(void) 
+{
+	int n,l,r;
+  clrscr();
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	printf("\n enter the left and right range:");
+	scanf("%d %d",&l,&r);
+	if(n>l && n<r)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+  getch();
+}
