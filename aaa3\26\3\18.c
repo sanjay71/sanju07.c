6d9c2187223d32864c1cd8f96c808c1ff6ed48f9
sanjay Anand.N <35688325+sanjay71@users.noreply.github.com>
2018-03-26 03:24:53 -0700

    aaa3\26\3\18.c

---
+++ aaa3\26\3\18.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+
+{
+
+	int n;
+  clrscr();
+
+	printf("enter the number:");
+
+	scanf("%d",&n);
+
+	if(n%13==0)
+
+	{
+
+		printf("\n yes");
+
+	}
+
+	else
+
+	{
+
+		printf("\n no");
+
+	}
+
+	return 0;
+  getch();
+
+}
