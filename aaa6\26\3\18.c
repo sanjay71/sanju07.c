c29cde57263a1961a6c3415682fec7281ad1a15d
sanjay Anand.N <35688325+sanjay71@users.noreply.github.com>
2018-03-26 03:34:20 -0700

    aaa6\26\3\18.c

---
+++ aaa6\26\3\18.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int num,rem,rev=0;
+  clrscr();
+	printf("enter the number:");
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		rem=num%10;
+		rev=(rev*10)+rem;
+		num=num/10;
+	}
+	num=rev;
+	while(num!=0)
+	{
+		rem=num%10;
+		if(rem%2==1)
+		{
+		printf("%d ",rem);
+		}
+		num=num/10;
+	}
+	return 0;
+getch();
+}
