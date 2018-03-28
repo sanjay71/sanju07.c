9861d9d131de879d334b740e80a70b52d9a7fdb0
sanjay Anand.N <35688325+sanjay71@users.noreply.github.com>
2018-03-28 15:45:47 +0530

    z2\28\3\18.c

---
+++ z2\28\3\18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	float l,b,a;
+  clrscr();
+	printf("enter the length and breadth values of the farm:");
+	scanf("%f %f",&l,&b);
+	a=l*b;
+	printf("\n%0.5f",a);
+	return 0;
+  getch();
+}
