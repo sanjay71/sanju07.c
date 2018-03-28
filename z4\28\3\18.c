3eddb4e142e420a2c2d51e6ee02b6b5f3922be01
sanjay Anand.N <35688325+sanjay71@users.noreply.github.com>
2018-03-28 15:57:01 +0530

    z4\28\3\18.c

---
+++ z4\28\3\18.c
@@ -0,0 +1,13 @@
+s (9 sloc)  133 Bytes
+#include <stdio.h>
+
+int main(void) 
+clrscr();
+{
+	char ch;
+	printf("\nenter the character");
+	scanf("%c",&ch);
+	printf("\n%d",ch);
+	return 0;
+  getch();
+}
